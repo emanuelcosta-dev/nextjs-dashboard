76ea346dc0b1701f838bd794fd3ef6a43a439a55
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-11 20:14:05 +0000

    Added demo component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import DemoBox from '../ui/demo-box';
 
 export default function LoginPage() {
   return (
@@ -11,6 +12,7 @@ export default function LoginPage() {
           </div>
         </div>
         <LoginForm />
+        <DemoBox />
       </div>
     </main>
   );
---
+++ app/ui/demo-box.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function DemoBox() {
+  return (
+    <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-4">
+      <p>
+        <b>Demo:</b>
+      </p>
+      <p>user@nextmail.com</p>
+      <p>123456</p>
+    </div>
+  );
+}
